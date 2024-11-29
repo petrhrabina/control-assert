4c18bb9167230118eccc912c0de844389d16e5ac
petrhrabina <petr.hrabina@gmail.com>
2024-11-29 18:57:32 +0100

    breaking: initial commit

---
+++ .github/PULL_REQUEST_TEMPLATE.md
@@ -0,0 +1,32 @@
+# Pull Request Title
+<!-- Provide a concise and descriptive title for this pull request -->
+<!--
+major:
+minor:
+feature:
+patch:
+fix:
+ -->
+## Description
+<!-- Provide a detailed description of the changes in this pull request. What was added, modified, or removed? -->
+
+## Type of Change
+-  [ ] Bugfix (fixes an issue)
+-  [ ] New feature (adds functionality)
+-  [ ] Refactoring (code change that doesn't affect functionality)
+-  [ ] Documentation (changes or additions to documentation)
+
+## Issues Resolved
+<!-- List any issues this pull request resolves, e.g., closes #123 -->
+
+## How to Test
+<!-- Provide instructions on how to test or verify the changes -->
+
+## Additional Notes
+<!-- Add any other notes that should be considered during review -->
+
+## Checklist
+-  [ ] Has the change been tested?
+-  [ ] Has the change been documented?
+-  [ ] Are all tests passing?
+
---
+++ src/Assert.php
@@ -0,0 +1,61 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Consistence;
+
+class Assert
+{
+
+    public static function true(
+        mixed $value,
+        string $description = 'Expected value to be true'
+    ): void {
+        assert(
+            $value === true,
+            $description
+        );
+    }
+
+    public static function false(
+        bool $value,
+        string $description = 'Expected value to be false'
+    ): void {
+        assert(
+            $value === false,
+            $description
+        );
+    }
+
+    public static function instanceOf(
+        mixed $object,
+        string $className,
+        string $description = 'Expected object to be instance of'
+    ): void {
+        if (!class_exists($className) && !interface_exists($className)) {
+            throw new \InvalidArgumentException(
+                sprintf('Class or interface "%s" does not exist', $className)
+            );
+        }
+
+        if (!is_object($object)) {
+            throw new \InvalidArgumentException(
+                sprintf(
+                    'Expected an object, got %s',
+                    get_debug_type($object)
+                )
+            );
+        }
+
+        assert(
+            $object instanceof $className,
+            sprintf(
+                '%s [%s], got instance of [%s]',
+                $description,
+                $className,
+                get_class($object)
+            )
+        );
+    }
+
+}
